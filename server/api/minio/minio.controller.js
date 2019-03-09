b8a955856212449c308c25c223cea52c6bee97de
ashishs <ashish.s@mobinexttech.com>
2019-03-09 20:19:43 +0530

    dddd

--- server/api/minio/minio.controller.js
+++ server/api/minio/minio.controller.js
@@ -19,7 +19,7 @@ const Minio = require('minio');
 const Users = require('../user/user.model');
 
 const minioClient = new Minio.Client({
-  endPoint: '103.86.177.84',
+  endPoint: 'env-8478623.mj.milesweb.cloud',
   useSSL: true,
   accessKey: process.env.MINIO_ACCESSKEY,
   secretKey: process.env.MINIO_SECRETKEY
